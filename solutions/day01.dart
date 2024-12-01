0ef88269700510ffe2fff14fa848fd7383473847
FDuhen <florian.duhen.pro@gmail.com>
2024-12-01 23:53:29 +0100

    feat: day 01 solutions

--- solutions/day01.dart
+++ solutions/day01.dart
@@ -1,39 +1,56 @@
+import 'dart:io';
+
 import '../utils/index.dart';
 
-/// Every day should extend [GenericDay] to have access to the corresponding
-/// input and a common interface.
-///
-/// Naming convention is set to pad any single-digit day with `0` to have proper
-/// ordering of files and correct mapping between input for days and the day
-/// files.
 class Day01 extends GenericDay {
-  // call the superclass with an integer == today´s day
   Day01() : super(1);
 
-  /// The [InputUtil] can be accessed through the superclass variable `input`. \
-  /// There are several methods in that class that parse the input in different
-  /// ways, an example is given below
-  ///
-  /// The return type of this is `dynamic` for [GenericDay], so you can decide
-  /// on a day-to-day basis what this function should return.
   @override
-  List<int> parseInput() {
-    final lines = input.getPerLine();
-    // exemplary usage of ParseUtil class
-    return ParseUtil.stringListToIntList(lines);
+  String parseInput() {
+    final file = File('input/aoc01.txt');
+    try {
+      final content = file.readAsStringSync();
+      return content;
+    } catch (e) {
+      return '';
+    }
   }
 
-  /// The `solvePartX` methods always return a int, the puzzle solution. This
-  /// solution will be printed in main.
   @override
   int solvePart1() {
-    // TODO implement
-    return 0;
+    final content = parseInput();
+    final lines = content.split('\n')..removeLast();
+    final pairs = lines.map((e) => e.split('   '),).toList();
+
+    final firstColumn = pairs.map((e) => e[0],).toList()..sort();
+    final secondColumn = pairs.map((e) => e[1],).toList()..sort();
+
+    var diffs = 0;
+    for (var i = 0; i < firstColumn.length; i++) {
+      final diff = int.parse(firstColumn[i]) - int.parse(secondColumn[i]);
+      diffs += diff.abs();
+    }
+
+    return diffs;
   }
 
   @override
   int solvePart2() {
-    // TODO implement
-    return 0;
+    final content = parseInput();
+    final lines = content.split('\n')..removeLast();
+    final pairs = lines.map((e) => e.split('   '),).toList();
+
+    final firstColumn = pairs.map((e) => e[0],).toList()..sort();
+    final secondColumn = pairs.map((e) => e[1],).toList()..sort();
+
+    var count = 0;
+    for (var i = 0; i < firstColumn.length; i++) {
+      final currentLeft = int.parse(firstColumn[i]);
+      final occurencesInRight = secondColumn.where((e) => int.parse(e) == currentLeft).length;
+      count += occurencesInRight*currentLeft;
+    }
+
+    return count;
   }
 }
+
