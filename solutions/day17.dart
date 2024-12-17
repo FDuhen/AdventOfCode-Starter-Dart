679bbcd146d2b506068be269d2349b98cbe91027
FDuhen <florian.duhen.pro@gmail.com>
2024-12-17 20:24:37 +0100

    feat: solution of day 17 (izi pizi just have to read)

---
+++ solutions/day17.dart
@@ -0,0 +1,131 @@
+import 'dart:io';
+
+import '../utils/index.dart';
+import 'day07.dart';
+
+enum OPCode { adv, bxl, bst, jnz, bxc, out, bdv, cdv }
+
+class Program {
+  int a;
+  int b;
+  int c;
+  List<int> program;
+  int currentInstruction = 0;
+  List<int> outputs = [];
+
+  Program(this.program, this.a, this.b, this.c);
+}
+
+class Day17 extends GenericDay {
+  Day17() : super(17);
+
+  @override
+  Program parseInput() {
+    final file = File('input/aoc17.txt');
+    final content = file.readAsStringSync();
+
+    // Parse the input
+    var a = 0;
+    var b = 0;
+    var c = 0;
+    var program = <int>[];
+
+    for (var line in content.split('\n')) {
+      line = line.trim(); // Remove whitespace
+      if (line.startsWith('Register A:')) {
+        a = int.parse(line.split(':')[1].trim());
+      } else if (line.startsWith('Register B:')) {
+        b = int.parse(line.split(':')[1].trim());
+      } else if (line.startsWith('Register C:')) {
+        c = int.parse(line.split(':')[1].trim());
+      } else if (line.startsWith('Program:')) {
+        program = line.split(':')[1].trim().split(',').map(int.parse).toList();
+      }
+    }
+
+    // Initialize the Program class
+    return Program(program, a, b, c);
+  }
+
+
+  void executeOPCode(OPCode code, int operand, Program program) {
+    switch(code) {
+      case OPCode.adv:
+        final numerator = program.a;
+        final denominator = 2.pow(getValuePerOperand(operand, program));
+
+        program.a = (numerator/denominator).truncate();
+      case OPCode.bxl:
+        program.b = program.b^operand;
+      case OPCode.bst:
+        program.b = getValuePerOperand(operand, program) % 8;
+      case OPCode.jnz:
+        if (program.a != 0) {
+          program.currentInstruction = operand;
+          return;
+        }
+      case OPCode.bxc:
+        program.b = program.b ^ program.c;
+      case OPCode.out:
+        program.outputs.add(getValuePerOperand(operand, program)%8);
+      case OPCode.bdv:
+        final numerator = program.a;
+        final denominator = 2.pow(getValuePerOperand(operand, program));
+
+        program.b = (numerator/denominator).truncate();
+      case OPCode.cdv:
+        final numerator = program.a;
+        final denominator = 2.pow(getValuePerOperand(operand, program));
+
+        program.c = (numerator/denominator).truncate();
+    }
+
+    print(program.currentInstruction);
+    program.currentInstruction += 2;
+  }
+
+  int getValuePerOperand(int operand, Program program) {
+    switch (operand) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+        return operand;
+      case 4:
+        return program.a;
+      case 5:
+        return program.b;
+      case 6:
+        return program.c;
+      case 7:
+        throw Exception('Invalid operand $operand');
+      default:
+        throw Exception('Out of bounds operand $operand');
+    }
+  }
+
+  @override
+  int solvePart1() {
+    final program = parseInput();
+
+    try {
+      while(true) {
+        final instruction = program.program[program.currentInstruction];
+        final code = OPCode.values[instruction];
+        final operand = program.program[program.currentInstruction + 1];
+
+        executeOPCode(code, operand, program);
+      }
+    } catch (e) {
+      final result = program.outputs.join(',');
+      print('SOLUTION IS : $result');
+    }
+
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+    return 0;
+  }
+}
